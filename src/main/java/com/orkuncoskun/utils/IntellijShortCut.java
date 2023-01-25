6f648045d874fbf76c59c16cf578dd802e4d05f9
Orkun Coskun <coskunorkun1@gmail.com>
2023-01-25 14:22:09 +0300

    shortcuts

---
+++ src/main/java/com/orkuncoskun/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.orkuncoskun.utils;
+
+public class IntellijShortCut {
+    //Ctrl + alt + s ==> settings
+    //Alt + Ins ==> constructor, toString, getter setter
+    //Ctrl + / ==>  single comment
+    //Ctrl + shift + / ==> multiple comment
+}
