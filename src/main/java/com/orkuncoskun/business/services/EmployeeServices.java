d621bdff0685fa5cfaa44b69dff1b954e9a3ec4d
Orkun Coskun <coskunorkun1@gmail.com>
2023-02-01 13:48:47 +0300

    EmployeeServices (interface)

---
+++ src/main/java/com/orkuncoskun/business/services/EmployeeServices.java
@@ -0,0 +1,22 @@
+package com.orkuncoskun.business.services;
+
+import com.orkuncoskun.business.dto.EmployeeDto;
+import com.orkuncoskun.data.entity.EmployeeEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmployeeServices {
+
+    //CRUD
+    public List<EmployeeDto> getAllEmployees();
+    public EmployeeDto createEmployee(EmployeeDto employeeDto);
+    public ResponseEntity<EmployeeDto> getEmployeeById(Long id) throws Throwable;
+    public ResponseEntity<EmployeeDto> updateEmployee(Long id, EmployeeDto employeeDto) throws Throwable;
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(Long id) throws Throwable;
+
+    //model mapper
+    public EmployeeDto EntityToDto(EmployeeEntity employeeEntity);
+    public EmployeeEntity DtoToEntity(EmployeeDto employeeDto);
+}
