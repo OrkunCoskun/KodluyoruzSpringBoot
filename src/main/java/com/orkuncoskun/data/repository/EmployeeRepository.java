df98e7f03b22c05d7dc5578cde643663a4ac1053
Orkun Coskun <coskunorkun1@gmail.com>
2023-02-01 13:49:26 +0300

    revize

--- src/main/java/com/orkuncoskun/data/repository/EmployeeRepository.java
+++ src/main/java/com/orkuncoskun/data/repository/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.orkuncoskun.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepository > CrudRepository
 @Repository
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends CrudRepository {
 }
