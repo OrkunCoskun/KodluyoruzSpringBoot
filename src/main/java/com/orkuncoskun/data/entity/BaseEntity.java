a8482d8819e186164da5b08c2a1bd24553579b85
Orkun Coskun <coskunorkun1@gmail.com>
2023-02-02 11:55:56 +0300

    Auditing (Kim ne zaman ekledi güncelledi)

--- src/main/java/com/orkuncoskun/data/entity/BaseEntity.java
+++ src/main/java/com/orkuncoskun/data/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.orkuncoskun.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +13,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
-@MappedSuperclass
+//lombok
 @Getter
 @Setter
+
+//hibernate jpa
+@MappedSuperclass
+
+//Auditing
 @EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_date, updated_date"}, allowGetters = true)
 public class BaseEntity {
 
     @Id
